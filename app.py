c46165463e260d0ffcbc7330e3c02e01f58c3e7f
efraAI <72665635+efraAI@users.noreply.github.com>
2021-01-27 21:48:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def webhook():
                         target_docs = []
                         input_tokens = set()
                         target_tokens = set()
-                        for line in pairs[:556]:
+                        for line in pairs[:3000]:
                           input_doc, target_doc = line[0], line[1]
                           # Appending each input sentence to input_docs
                           input_docs.append(input_doc)
